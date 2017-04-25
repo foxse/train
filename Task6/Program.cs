b8a31a5484e8aee4026837b5e174e5b795aac09d
Fox <foxse@bk.ru>
2017-04-25 20:18:08 +0300

    Add project files.

---
+++ Task6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace task6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint stackSize;
+
+            string stdi = Console.ReadLine();
+            uint.TryParse(stdi, out stackSize);
+
+            var stack = new Task6.SuperStack(stackSize);
+
+            int a;
+            int b;
+
+            while (stdi != "q")
+            {
+                stdi = Console.ReadLine();
+                string[] spl = stdi.Split(' ');
+                switch (spl[0])
+                {
+                    case "push":
+                        int.TryParse(spl[1], out a);
+                        stack.Push(a);
+                        break;
+                    case "pop":
+                        stack.Pop();
+                        break;
+                    case "inc":
+                        int.TryParse(spl[1], out a);
+                        int.TryParse(spl[2], out b);
+                        stack.Inc(a, b);
+                        break;
+                    default:
+                        return;
+                }
+                Console.WriteLine(stack.Peek());
+            }
+        }
+
+
+    }
+}
---
+++ Task6/SuperStack.cs
@@ -0,0 +1,48 @@
+﻿namespace Task6
+{
+    class SuperStack
+    {
+        static int cursor = 0;
+        static int[] stack;
+
+        public SuperStack(uint size)
+        {
+            stack = new int[size];
+        }
+
+        public void Push(int k)
+        {
+            if (cursor >= stack.Length)
+                return;
+
+            stack[cursor++] = k;
+        }
+
+        public void Pop()
+        {
+            if (cursor > 0)
+            {
+                cursor--;
+            }
+        }
+
+        public void Inc(int a, int b)
+        {
+            for (int i = 0; (i < a && i < cursor); i++)
+            {
+                stack[i] += b;
+            }
+        }
+
+        public string Peek()
+        {
+            if (cursor == 0)
+            {
+                return "EMPTY";
+            }
+
+            return stack[cursor - 1].ToString();
+        }
+
+    }
+}
